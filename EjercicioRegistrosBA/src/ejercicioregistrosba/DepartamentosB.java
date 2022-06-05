3e50bd1974775b3d74c92c97ec18e08dbda92942
Brandon <bampereza@miumg.edu.gt>
2022-06-05 15:15:17 -0600

    ClonEjercicio

---
+++ EjercicioRegistrosBA/src/ejercicioregistrosba/DepartamentosB.java
@@ -0,0 +1,163 @@
+
+package ejercicioregistrosba;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author BRANDON
+ */
+public class DepartamentosB {
+     /* ArrayList<AlumnosB> listAlumnos = new ArrayList();*/
+     List<DepartamentosB> listDepa = new ArrayList<DepartamentosB>();
+    
+    
+     Scanner sc= new Scanner(System.in);
+    
+     
+    int[] vector;
+    private double[] matriz;
+    private int indiceVector;
+    String listaA;
+    String verLista;
+    int verLista2;
+        
+     
+     String verNombreA;
+     String verNombreB;
+     String addAlumnosB;
+     
+      //constructor
+
+
+    String nombreD;
+    int numeroM;
+    int registroA;
+    String nombreC;
+    
+    
+    
+    public DepartamentosB() {
+        
+    }
+    
+    
+    public DepartamentosB(String nombreD, int numeroM, String nombreC, int registroA) {
+       
+        this.nombreD = nombreD ;
+        this.numeroM =numeroM;
+        this.nombreC=nombreC;
+        this.registroA=registroA;
+        
+    }
+
+    public String getNombreD() {
+        return nombreD;
+    }
+
+    public void setNombreD(String nombreD) {
+        this.nombreD = nombreD;
+    }
+
+   
+
+    public int getNumeroM() {
+        return numeroM;
+    }
+
+    public void setNumeroM(int numeroM) {
+        this.numeroM = numeroM;
+    }
+
+    public String getNombreC() {
+        return nombreC;
+    }
+
+    public void setNombreC(String nombreC) {
+        this.nombreC = nombreC;
+    }
+
+    public int getRegistroA() {
+        return registroA;
+    }
+
+    public void setRegistroA(int registroA) {
+        this.registroA = registroA;
+    }
+    
+    
+
+ 
+    
+    public void listaDepa(){
+                        
+                        System.out.println("----------------------------------------------");  
+                        System.out.println("-----Ingrese el numero del departamento: -----");
+                        System.out.println("----------------------------------------------");
+                          registroA=sc.nextInt();
+        
+                        System.out.println("--------------------------------------------");  
+                        System.out.println("---Ingrese el nombre del Departamento: -----");
+                        System.out.println("--------------------------------------------");
+                          nombreD=sc.next();
+                        
+                         
+                        System.out.println("------------------------------------------------------");  
+                        System.out.println("-----Ingrese la cantidad de municipios que tiene: ----");
+                        System.out.println("------------------------------------------------------"); 
+                         numeroM= sc.nextInt();
+                         
+                         
+                        System.out.println("------------------------------------------------");  
+                        System.out.println("-----Ingrese la cabecera del departamento: -----");
+                        System.out.println("------------------------------------------------"); 
+                         nombreC=sc.next();
+                         
+                         
+        
+        listDepa.add(new DepartamentosB(nombreD,numeroM,nombreC, registroA));
+       
+        //return verLista;
+    }
+    
+    public void lista(){
+
+        System.out.println("----------------------------------");
+        System.out.println("-------Lista Departamentos--------");
+        System.out.println("----------------------------------");
+        System.out.println("");
+        //recorido for por Ã­ndice
+        for (int i=0; i<listDepa.size(); i++) {
+            System.out.println("El departamento se registro con el numero: " + listDepa.get(i).getRegistroA());
+            System.out.println("Nombre el departamento registrado es: " + listDepa.get(i).getNombreD());
+            System.out.println("La cantidad de municipios que tiene es: " + listDepa.get(i).getNumeroM());
+            System.out.println("La cabecera del departamento es: " + listDepa.get(i).getNombreC());
+        }
+        
+        
+    }
+    
+    public void mostrarDepa(){
+		
+          Scanner teclado = new Scanner (System.in);
+          int indice;
+          int registroB =0;
+          System.out.println("Verificar registro del departamento.");
+           registroB= teclado.nextInt();
+          indice = listDepa.indexOf(registroB);
+          if (indice != registroA){
+                      System.out.println("El departamento registrado con este numero es: "+nombreD);
+          }else {
+              System.out.println("Este registro del departamento no se encuentra");
+
+          }
+    }
+    
+     public void clearListDepa(){
+      listDepa.clear();
+   }
+        
+}
+
---
+++ EjercicioRegistrosBA/src/ejercicioregistrosba/EjercicioRegistrosBA.java
@@ -0,0 +1,189 @@
+//Brandon Isaias AmpÃ©rez Aldana 0909-21-4628
+package ejercicioregistrosba;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author BRANDON
+ */
+public class EjercicioRegistrosBA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+      Scanner sc= new Scanner(System.in);
+         DepartamentosA ejerciciosListasB= new DepartamentosA();
+         EjercicioRegistrosBA departamentosA=new EjercicioRegistrosBA();
+         DepartamentosB departamentosB =new DepartamentosB();
+         
+         
+          int opcion =0;
+          
+         do{
+              System.out.println("-------------------------------------------------------------------");
+              System.out.println("-------Bienvenido al programa de registro de DepartamentosBA-------");
+              System.out.println("-------------------------------------------------------------------");
+              System.out.println("Seleccione la opcion de registro que desee utilizar: ");
+              System.out.println("{1) Registro de datos con arrays"); 
+              System.out.println("{2) Registro de datos con listas"); 
+              System.out.println("{3) MostrarHistorial Arrays");
+              System.out.println("{4) MostrarHistorial Listas ");
+              System.out.println("{5) Limpiar Lista ");
+              System.out.println("{0) Salir");
+              
+              
+              System.out.println("Ingrese la opcion que desea usar: ");
+                  opcion =sc.nextInt();
+                  
+              switch(opcion){
+                 
+                 case 1:
+                     
+                     int opcionA = 0;
+                     
+                     do{
+                         
+                       System.out.println("-------------------------------------------------------------");
+                       System.out.println("----Bienvenido usuario seleccione la opcion a registrar: ----");
+                       System.out.println("-------------------------------------------------------------");
+                       System.out.println("(1) Registrar departamentos");
+                       System.out.println("(2) Buscar departamento por numero"); 
+                       System.out.println("(3) Buscar cabecera departamental"); 
+                       System.out.println("(4) Mostrar los departamentos por posiciones impares");
+                       System.out.println("(0) Volver al inicio");
+                       
+                     System.out.println("Ingrese la opcion que desea usar: ");
+                     opcionA =sc.nextInt();
+                     
+ 
+              switch(opcionA){
+
+                case 1:
+                     
+                     int registroA=0;
+                     String nombreA="";
+                     int numeroB=0;
+                     String nombreB="";
+                     
+                    
+                     
+                     ejerciciosListasB.datosDepa2();
+                     
+                         
+                      
+                          
+
+	          break;
+                  
+                  case 2:
+                   
+                    ejerciciosListasB.mostrarDatosDepartamento();
+                         
+      
+	          break;
+                  
+                 case 3:
+                      
+                     ejerciciosListasB.mostrarDatosCabecera();
+                     
+	          break;
+                  
+                  case 4:
+                      
+                     ejerciciosListasB.PosicionImpar();
+                     
+	          break;
+                  
+  
+                case 0:
+                     
+	          break;
+                }
+              
+             }while(opcionA!=0);
+   
+             break;
+             
+                case 2:
+                    
+                     
+                     int opcionAB = 0;
+                     
+                     do{
+                         
+                       System.out.println("-------------------------------------------------------------");
+                       System.out.println("----Bienvenido usuario seleccione la opcion a registrar: ----");
+                       System.out.println("-------------------------------------------------------------");
+                       System.out.println("(1) Registrar departamentos");
+                       System.out.println("(2) Buscar departamento por numero"); 
+                       System.out.println("(0) Volver al inicio");
+                       
+                     System.out.println("Ingrese la opcion que desea usar: ");
+                     opcionAB =sc.nextInt();
+                     
+ 
+              switch(opcionAB){
+
+                case 1:
+                      String nombreD="";
+                      int numeroM=0;
+                      String nombreC="";
+                      int registroA=0;
+ 
+                        departamentosB.listaDepa();
+                    
+                          
+
+	          break;
+                  
+                  case 2:
+                      
+                     departamentosB.mostrarDepa();
+                     
+	          break;
+                  
+                  
+   
+                case 0:
+                     
+	          break;
+                }
+              
+             }while(opcionAB!=0);
+   
+             break;
+                     
+                 
+                case 3:
+                  
+                           ejerciciosListasB.mostrarDatos();
+
+                 break;
+                 
+                case 4:
+                          departamentosB.lista();
+                       
+                     
+                 break;
+                 
+                  case 5:
+                          departamentosB.clearListDepa();
+                       
+                     
+                 break;
+                
+                 
+                case 0:
+                              
+                 break;
+                 
+              }
+         
+         }while(opcion!=0);        
+    
+         System.out.println("El programa a finalizado");
+    }
+}
